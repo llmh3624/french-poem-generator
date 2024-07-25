dd34228aadfd6e9cb394adbe34324291ebb30772
Laura Moragues <llmh3624@gmail.com>
2024-07-26 00:24:55 +0200

    Added JS API functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log("poem generated");
+  console.log(response.data.answer);
 
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "a3c53b47tec350ef3aeodd4ca5138dbc";
+  let context =
+    "You are a romantic french Poet expert who writes short poems. You have to write a 4 line poem in basic HTML and each line should be seperated by a <br /> except for the last line. Do not print a title just the poem.";
+  let prompt = `Generate a french poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
