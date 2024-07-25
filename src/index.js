7584d9810c41551c4a6a5f665040c092bd2f8fbd
Laura Moragues <llmh3624@gmail.com>
2024-07-26 00:46:18 +0200

    Added user experience

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 function displayPoem(response) {
-  console.log("poem generated");
-  console.log(response.data.answer);
-
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -16,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "a3c53b47tec350ef3aeodd4ca5138dbc";
   let context =
-    "You are a romantic french Poet expert who writes short poems. You have to write a 4 line poem in basic HTML and each line should be seperated by a <br /> except for the last line. Do not print a title just the poem.";
+    "You are a romantic french Poet expert who writes short poems. You have to write a 4 line poem in basic HTML, inside a <p></p> and each line should be seperated by a <br /> except for the fourth line. Do not print a title just the poem. Make sure to follow the user instructions.";
   let prompt = `Generate a french poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
